4f86cba1cf2bfbe98a2062f24afc4438974f1c9e
Bezaleel Ashefor <knightbenax@gmail.com>
2016-06-17 18:41:43 +0100

    SocialShare

--- app/scripts/tracklogic.js
+++ app/scripts/tracklogic.js
@@ -345,6 +345,13 @@ function showPreview(element) {
     NProgress.start();
     var input = element;
 
+    var rotation = {
+      1: 'rotate(0deg)',
+      3: 'rotate(180deg)',
+      6: 'rotate(90deg)',
+      8: 'rotate(270deg)'
+    };
+
     //let's hold the files in Angular
     var scope = angular.element(document.querySelector(".hero")).scope();
       scope.$apply(function () {
@@ -377,6 +384,10 @@ function showPreview(element) {
                     //t = input.files[i].type,                           // ext only: // file.type.split('/')[1],
                     //n = input.files[i].name,
                     //s = ~ ~(file.size / 1024) + 'KB';
+                    //var base64 = oFREvent.target.result.replace(/^.*?,/,'');
+	                  //var binary = atob(base64);
+                    //var exif = EXIF.readFromBinaryFile(new BinaryFile(binary));
+                    //alert(exif);
                     holder.src = oFREvent.target.result;
                     //showTracker();
                     $('#user_blob').faceDetection({
@@ -401,3 +412,107 @@ function showPreview(element) {
     }
 
 }
+
+
+var rotation = {
+  1: 'rotate(0deg)',
+  3: 'rotate(180deg)',
+  6: 'rotate(90deg)',
+  8: 'rotate(270deg)'
+};
+
+function _arrayBufferToBase64( buffer ) {
+  var binary = ''
+  var bytes = new Uint8Array( buffer )
+  var len = bytes.byteLength;
+  for (var i = 0; i < len; i++) {
+    binary += String.fromCharCode( bytes[ i ] )
+  }
+  return window.btoa( binary );
+}
+
+var orientation = function(file, callback) {
+
+  var fileReader = new FileReader();
+  fileReader.onloadend = function() {
+    var base64img = "data:"+file.type+";base64," + _arrayBufferToBase64(fileReader.result);
+    var scanner = new DataView(fileReader.result);
+    var idx = 0;
+    var value = 1; // Non-rotated is the default
+    if(fileReader.result.length < 2 || scanner.getUint16(idx) != 0xFFD8) {
+      // Not a JPEG
+      if(callback) {
+        callback(base64img, value);
+      }
+      return;
+    }
+    idx += 2;
+    var maxBytes = scanner.byteLength;
+    while(idx < maxBytes - 2) {
+      var uint16 = scanner.getUint16(idx);
+      idx += 2;
+      switch(uint16) {
+        case 0xFFE1: // Start of EXIF
+          var exifLength = scanner.getUint16(idx);
+          maxBytes = exifLength - idx;
+          idx += 2;
+          break;
+        case 0x0112: // Orientation tag
+          // Read the value, its 6 bytes further out
+          // See page 102 at the following URL
+          // http://www.kodak.com/global/plugins/acrobat/en/service/digCam/exifStandard2.pdf
+          value = scanner.getUint16(idx + 6, false);
+          maxBytes = 0; // Stop scanning
+          break;
+      }
+    }
+    if(callback) {
+      callback(base64img, value);
+    }
+  }
+  fileReader.readAsArrayBuffer(file);
+};
+
+
+function changeFile(element){
+  NProgress.start();
+  var file = $(element)[0].files[0];
+  if(file) {
+    orientation(file, function(base64img, value) {
+      $('#user_blob').attr('src', base64img);
+      console.log(rotation[value]);
+      var rotated = $('#user_blob').attr('src', base64img);
+      if(value) {
+        rotated.css('transform', rotation[value]);
+        $('#user_blob').faceDetection({
+           complete: function (faces) {
+              //console.log(faces.length);
+              completed(faces);
+           }
+        });
+      } else {
+        $('#user_blob').faceDetection({
+           complete: function (faces) {
+              //console.log(faces.length);
+              completed(faces);
+           }
+        });
+      }
+      /*$('#user_blob').faceDetection({
+         complete: function (faces) {
+            //console.log(faces.length);
+            completed(faces);
+         }
+      });*/
+      NProgress.done();
+
+    });
+  }
+}
+
+/*$(function() {
+
+  $('#file').change(function() {
+
+  });
+});*/
